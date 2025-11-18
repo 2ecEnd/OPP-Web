74ae25e39497a5321cc7bb6c7577f7984ee23213
Several_SIZE <kner647@gmail.com>
2025-11-18 23:55:37 +0700

    Added the ability to add tasks

--- javaScript/AddTaskContextMenu.js
+++ javaScript/AddTaskContextMenu.js
@@ -2,7 +2,7 @@ class AddTaskContextMenu{
     constructor(){
         this.contextMenu = document.getElementById('add-task-context-menu');
         this.closeContextMenuButton = document.getElementById('close-context-menu-button');
-        this.form = document.getElementById('add-task-context-menu');
+        this.form = document.getElementById('add-task-form');
         this.overlay = document.getElementById('overlay');
         this.canvas = canvas;
 
@@ -15,7 +15,42 @@ class AddTaskContextMenu{
     }
 
     handleSubmit(event){
-        
+        event.preventDefault();
+
+        const newTask = document.createElement('div');
+
+        const title = document.getElementById('formTitleInput').value;
+        const description = document.getElementById('formDescriptionInput').value;
+        const hasDeadline = document.getElementById('formDeadLineCheck').checked;
+        const deadline = document.getElementById('formDeadLineInput').value;
+        const currentDate = new Date().toLocaleDateString('ru-RU');
+
+        newTask.className = 'draggable task';
+        newTask.style.left = 0 + 'px';
+        newTask.style.top = 0 + 'px';
+        newTask.id = crypto.randomUUID();
+        newTask.innerHTML = `
+            <div class="title task-block">
+                <p>${title}</p>
+            </div>
+            <div class="description task-block">
+                <p>${description}</p>
+            </div>
+            <div class="create-time task-block">
+                <p>Дата добавления: ${currentDate}</p>
+            </div>
+            <div class="assigned-person task-block">
+                <p>Ответственный: name</p>
+            </div>
+            <div class="dead-line task-block">
+                <p>Дедлайн: ${deadline}</p>
+            </div>
+        `;
+
+        this.canvas.canvas.appendChild(newTask);
+
+        this.form.reset();
+        this.showSelf();
     }
 
     showSelf(){
