a9668c019a741fc7092d381e1f716ce1bf10d197
Several_SIZE <kner647@gmail.com>
2025-11-18 22:53:03 +0700

    added context menu

--- javaScript/AddTaskButton.js
+++ javaScript/AddTaskButton.js
@@ -1,16 +1,18 @@
 class AddTaskButton{
     constructor(){
         this.button = document.getElementById('add-task-button');
+        this.contextMenu = null;
 
         this.init();
     }
 
     init(){
-        this.button.addEventListener('click', this.openContextMenu.bind(this))
+        this.button.addEventListener('click', this.openContextMenu.bind(this));
+        this.contextMenu = new AddTaskContextMenu();
     }
 
     openContextMenu(){
-        
+        this.contextMenu.showSelf();
     }
 }
 
--- javaScript/AddTaskContextMenu.js
+++ javaScript/AddTaskContextMenu.js
@@ -1,7 +1,25 @@
 class AddTaskContextMenu{
     constructor(){
         this.contextMenu = document.getElementById('add-task-context-menu');
+        this.closeContextMenuButton = document.getElementById('close-context-menu-button');
+        this.form = document.getElementById('add-task-context-menu');
+        this.overlay = document.getElementById('overlay');
+        this.canvas = canvas;
 
         this.init();
+    }
+
+    init(){
+        this.form.addEventListener('submit', this.handleSubmit.bind(this));
+        this.closeContextMenuButton.addEventListener('click', this.showSelf.bind(this));
+    }
+
+    handleSubmit(event){
+        
+    }
+
+    showSelf(){
+        this.contextMenu.classList.toggle('active');
+        this.overlay.classList.toggle('active');
     }
 }
