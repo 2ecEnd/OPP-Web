0cdaecf758c72338734d77c4bc1155d39830d250
Several_SIZE <kner647@gmail.com>
2025-12-03 00:14:33 +0700

    added connections removing

--- javaScript/Subject.js
+++ javaScript/Subject.js
@@ -10,7 +10,22 @@ class Subject{
     }
 
     deleteTask(id){
+        const taskToDelete = this.tasks.find(task => task.id === id);
+        canvas.links.forEach(link => {
+            if (link.startTask === taskToDelete || link.endTask === taskToDelete){
+                link.line.remove();
+            }
+        });
+
+        canvas.links = canvas.links.filter(link => 
+            link.startTask !== taskToDelete && link.endTask !== taskToDelete
+        );
+
         this.tasks = this.tasks.filter(task => task.id !== id);
+
+        this.tasks.forEach(task => {
+            task.dependsOn.filter(t => t.id !== taskToDelete.id);
+        });
     }
 
     getTask(id){
