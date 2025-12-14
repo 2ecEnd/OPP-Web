ed7a2752e32c2121364d3ea106cffa49d2903937
Several_SIZE <kner647@gmail.com>
2025-12-14 15:23:50 +0700

    Added visualization of the direction of dependence

--- javaScript/InitEditor.js
+++ javaScript/InitEditor.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', async function() {
     if(!user) await initUser();
-
+    
     const activeTab = toolbar.getActiveTab();
     const currentSubject = activeTab.type == "subject" ? await user.getSubjectById(activeTab.id) : new Subject("test", [], null, null);
     
-    console.log(await user.getSubjectById(activeTab.id));
     canvas = new Canvas(currentSubject);
     addTaskMenu = new AddTaskMenu();
     addTaskButton = new AddTaskButton();
