b4cfad95f5d66b1e5c8632c2cfa7fa8245c7729b
Several_SIZE <kner647@gmail.com>
2025-12-25 21:03:42 +0700

    Вынес

---
+++ javaScript/SubjectView.ts
@@ -0,0 +1,82 @@
+import { addSubjectMenu } from "./AddSubjectMenu.js";
+import type { Subject } from "./Subject.js";
+
+export class SubjectView{
+    private model: Subject;
+    public container: HTMLElement;
+
+    constructor(model: Subject){
+        this.model = model;
+        this.container = document.createElement('div');
+
+        this.createView();
+    }
+
+    openContextMenu(e: Event): void {
+        e.stopPropagation();
+
+        const moreVertButton: HTMLElement = this.container.querySelector('.more-vert-button')!;
+        const contextMenu: HTMLElement = this.container.querySelector('.subject-context-menu')!;
+        
+        moreVertButton.classList.toggle('active');
+        contextMenu.classList.toggle('active');
+    }
+
+    openEditMenu(e: Event): void{
+        e.stopPropagation();
+
+        addSubjectMenu.showSelf("edit", this.model);
+    }
+
+    createContextMenu(): HTMLElement {
+        const contextMenu: HTMLElement = document.createElement('div');
+        contextMenu.className = 'subject-context-menu subject-menu';
+        
+        const actions = [
+            { text: 'Изменить', handler: this.openEditMenu.bind(this) },
+            { text: 'Удалить', handler: this.model.deleteSubject.bind(this.model) }
+        ];
+
+        actions.forEach(({ text, handler}) => {
+            const button = document.createElement('button');
+            button.textContent = text;
+            
+            if (handler) {
+                button.addEventListener('click', handler);
+            }
+            
+            contextMenu.appendChild(button);
+        });
+
+        return contextMenu;
+    }
+
+    createView(): void {
+        const newElement: HTMLElement = document.createElement('div');
+        this.container = newElement;
+
+        newElement.classList.add('contentItem');
+        newElement.classList.add('subjectItem');
+        newElement.innerHTML = `
+            <div class="miniature"></div>
+            <div class="subject-info">
+                <p>Название: ${this.model.name}</p>
+                <p>Команда: </p>
+            </div>
+            <div class="more-vert-button">
+                <img src="../images/More vertical.svg" alt="">
+            </div>
+        `
+        newElement.setAttribute('data-type', 'subject');
+        newElement.setAttribute('data-id', this.model.id);
+
+        newElement.appendChild(this.createContextMenu());
+
+        newElement.querySelector('.more-vert-button')!.addEventListener('click', 
+            this.openContextMenu.bind(this));
+    }
+
+    updateView(): void {
+        this.container.querySelector('.subject-info')!.children[0]!.textContent = this.model.name;
+    }
+}
